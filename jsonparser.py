ca23d364013b11ffe28997c849998072171bf2c4
gzs15363 <yudayanga@163.com>
2020-05-27 10:13:40 +0800

    start

---
+++ jsonparser.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+class jsonparser():
+    def __init__(self):
+        self._data = {}
+
+    def __setitem__(self, key, value):
+
+
+    def __getitem__(self, item):
+
+
+    def loads(self, s):
+
+
+    def dumps(self, s):
+
+
+    def load_file(self, f):
+
+
+    def dump_file(self, f):
+
+
+    def load_dict(self, d):
+
+
+    def dump_dict(self, f):
+
+
+    def updata(self, d):
